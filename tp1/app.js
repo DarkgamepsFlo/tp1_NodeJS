7919ebc23f271bd938daa95cdac7bb6a7e4db6c7
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2023-01-27 11:25:53 +0100

    Add files via upload
    tp1 Fini

---
+++ tp1/app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// 8
+app.use("/", (req, res) => {
+    console.log(new Date().toJSON() + " " + req.url);
+})
+
+// 10
+
+const metrics = {
+    requestCount : {},
+    status : "healthy", // Route qui vont permettre de tester si l'appli est en ligne ou non
+};
+
+app.get('/metrics', (req, res, next) => {
+    metrics.uptime = '${process.uptime().toFixed(2)} seconds';
+    return res.json(metrics)
+})
+
+app.use((req, res, next) => {
+    const currentUrlRequestCount = metrics.requestCount[req.url];
+    metrics.requestCount[req.url] = currentUrlRequestCount
+        ? currentUrlRequestCount + 1 // Si on l'appelle pour la première fois, on initialise à 1
+        : 1; // Sinon on ajoute 1
+    return next(); // return : S'assurer que la fonction ne continu pas après
+});
+
+// 0
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// 1
+app.get('/welcome', (req, res) => {
+    res.send('Bienvenue sur le TP 1 du cours d architecture logiciel');
+})
+
+// 2
+app.get('/secret', (req, res) => {
+    res.status(401).send("Vous ne possédez pas les droits pour accéder à ma page secrète");
+})
+
+// 3
+app.get('/error', (req, res) => {
+    res.status(500).json({message : 'message'});
+})
+
+// 4
+app.get('/img', (req, res) => {
+    res.download("./Ramm.png");
+})
+
+// 5
+app.get('/redirectMe', (req, res) => {
+    res.redirect("http://extra.univ-littoral.fr/");
+})
+
+// 6
+app.get('/users/:name', (req, res) => {
+    res.send("Bienvenue sur la page de " + req.params.name)
+})
+  
+// 7
+app.get('/somme', function (req, res, next) {
+    const a = Number(req.query.a)
+    const b = Number(req.query.b)
+    const result = a + b
+    res.send(req.params.a + " + " + req.params.b + " = " + (parseInt(req.params.a) + parseInt(req.params.b)))
+    next()
+})
+
+// 9
+app.use((req, res) => {
+    res.status(404).setDefaultEncoding("Cette Page n'existe pas");
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
--- tp1/helloWorld.js
+++ tp1/helloWorld.js
@@ -1,11 +1,1 @@
-const express = require("express"); // Pour pouvoir importer quelque chose en JS
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res, next) => {
-    res.setDefaultEncoding("Hello worl");
-});
-
-app.listen(port, () => {
-    console.log('Exemple app listening on port ${port}');
-});
+console.log("Hello World");
