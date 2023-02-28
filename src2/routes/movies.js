da7bbf06b4e4b62a161b67791e16a0b68f990a58
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2023-02-28 21:36:45 +0100

    Delete src2 directory

--- src2/routes/movies.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {insertOneMovies, insertOneItem, updatestatus} = require("../controllers/movies");
-
-router.post("/insertMovie", insertOneMovies);
-router.post("/insertItem", insertOneItem);
-router.post("/updateStatus", updatestatus);
-
-module.exports = router;
--- src2/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {createUser, createWatchlist} = require("../controllers/users");
-
-router.post("/create", createUser);
-router.post("/createWatchlist", createWatchlist)
-
-module.exports = router;
