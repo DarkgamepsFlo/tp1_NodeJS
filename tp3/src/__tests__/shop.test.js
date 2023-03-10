416b9ea722142580a78456ecf6423b01d2cd4262
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2023-03-10 14:03:07 +0100

    Add files via upload
    Création des deux premières fonctions avec les commentaires ainsi que les tests unitaires

--- tp3/src/__tests__/shop.test.js
+++ tp3/src/__tests__/shop.test.js
@@ -1,8 +1,18 @@
-const { getUser } = require("../shop");
+const { getUser, getNumero, getInfoProduit } = require("../shop");
 
 const fakeUsers = require("../__mocks__/fakeUserValid.json");
+const usersAgeStr = require("../__mocks__/usersAgeStr.json");
+const fakeListProduct = require("../__mocks__/categorieProduit.json");
+const listStockNeg = require("../__mocks__/categorieProduitStockNeg.json");
+const listStockInt = require("../__mocks__/categorieInt.json");
+const listStockReal = require("../__mocks__/categorieReal.json");
+
+const RealUsers = require("../users.json");
+const RealProduct = require("../products.json");
 
 describe("shop.js", () => {
+
+  // Test pour la fonction getUser()
   describe("getUser", () => {
     it("Doit retourner le bon user", () => {
       const res = getUser(1, fakeUsers);
@@ -34,6 +44,66 @@ describe("shop.js", () => {
       expect(() => {
         getUser(-1, fakeUsers);
       }).toThrow("L'identifiant doit être un entier positif");
+    });
+  });
+
+  // Test pour la fonction getNumero
+  describe("getNumero", () => {
+    it("Doit retourner la liste avec le numéro de téléphone des personnes de 50 ans", () => {
+      const res = getNumero(RealUsers);
+      expect(res).toStrictEqual(["+63 791 675 8914", "+55 539 822 6581", "+62 406 110 4091"]);
+    }); 
+    it("Doit throw une erreur car la liste des utilisateur est du mauvais type", () => {
+      expect(() => {
+        getNumero("liste");
+      }).toThrow(
+        "La liste des utilisateur doit être un tableau contenant des utilisateurs"
+      );
+    });
+    it("Doit throw une erreur car la liste des utilisateur ne doit pas être vide", () => {
+      expect(() => {
+        getNumero([]);
+      }).toThrow("La liste des utilisateur est vide");
+    });
+    it("Doit throw une erreur car l'age ne peut pas être une chaine de caractère", () => {
+      expect(() => {
+        getNumero(usersAgeStr);
+      }).toThrow("L'age est un string");
+    });
+  });
+
+  // Test pour la fonction getInfoProduit
+  describe("getInfoProduit", () => {
+    it("Doit retourner la liste avec l'ensemble des produits classés par catégories'", () => {
+      const res = getInfoProduit(RealProduct);
+      expect(res).toStrictEqual(fakeListProduct);
+    }); 
+    it("Doit throw une erreur car la liste des produits est du mauvais type", () => {
+      expect(() => {
+        getInfoProduit("liste");
+      }).toThrow(
+        "La liste des produits doit être un tableau contenant des produits"
+      );
+    });
+    it("Doit throw une erreur car la liste des produits ne doit pas être vide", () => {
+      expect(() => {
+        getInfoProduit([]);
+      }).toThrow("La liste des produits est vide");
+    });
+    it("Doit throw une erreur car la liste des produits ne doit pas être vide", () => {
+      expect(() => {
+        getInfoProduit(listStockNeg);
+      }).toThrow("Le stock est négatif, vous devez des produits");
+    });
+    it("Doit throw une erreur car la liste des produits ne doit pas être vide", () => {
+      expect(() => {
+        getInfoProduit(listStockInt);
+      }).toThrow("La catégorie est un entier");
+    });
+    it("Doit throw une erreur car la liste des produits ne doit pas être vide", () => {
+      expect(() => {
+        getInfoProduit(listStockReal);
+      }).toThrow("La catégorie est un entier");
     });
   });
 });
--- tp3/src/shop.js
+++ tp3/src/shop.js
@@ -1,3 +1,8 @@
+const productsData = require("./products.json");
+const paniersData = require("./paniers.json");
+const usersData = require("./users.json");
+const _ = require("lodash");
+
 /**
  * @function
  * @param {number} id, id de l'utilisateur a rechercher
@@ -20,6 +25,65 @@ function getUser(id, usersData) {
     return usersData[id - 1];
 }
 
+/**
+ * @function 
+ * @param {List} usersData Liste d'utilisateurs
+ * @returns Return l'ensemble des numéros de téléphone de l'ensemble des personnes ayant plus de 50 ans
+ */
+function getNumero(usersData){
+  var listNum = [];
+  if (typeof(usersData) != typeof([]))
+    throw new Error ("La liste des utilisateur doit être un tableau contenant des utilisateurs");
+  if (usersData.length == 0)
+    throw new Error ("La liste des utilisateur est vide");
+  for(var i = 0; i < usersData.length; i++){
+    if(typeof(usersData[i].age) == typeof("lol"))
+      throw new Error ("L'age est un string");
+    if(usersData[i].age >= 50)
+      listNum.push(usersData[i].phone);
+  }
+  return listNum;
+}
+
+/**
+ * 
+ * @param {*} productsData Liste des produits
+ * @returns Return l'ensemble des produits, classés par catégorie, précisant le libelle et le stock
+ */
+function getInfoProduit(productsData){
+  var listProd = {};
+  if (typeof(productsData) != typeof([]))
+    throw new Error ("La liste des produits doit être un tableau contenant des produits");
+  if (productsData.length == 0)
+    throw new Error ("La liste des produits est vide");
+  
+  // Cette boucles permet d'ajouter des clés => valeur (= nom de la catégorie => Liste vide) pour chaque catégories pas encore présentes
+  for(i of productsData){
+    categ = i.category;
+    if((typeof(categ) == typeof(1)) || (typeof(categ) == typeof(1.0)))
+      throw new Error ("La catégorie est un entier");
+    if (productsData.indexOf(categ)===-1)
+      listProd[i.category] = [];
+  }
+
+  // Cette boucle va permettre d'ajouter le libelle du produit ainsi qu'un mot qui va changer selon la quantité de stock
+  for(j of productsData){
+    stock = j.stock;
+    if (stock < 0)
+      throw new Error ("Le stock est négatif, vous devez des produits");
+    if (stock <= 10 && stock >= 0)
+      stoock = "low"
+    if (stock > 10 && stock < 50)
+      stoock = "medium"
+    if (stock >= 50)
+      stoock = "hight"
+    listProd[j.category].push({libelle: j.title, dispo: stoock});
+  }
+  return listProd;
+}
+
 module.exports = {
   getUser,
+  getNumero,
+  getInfoProduit,
 };
