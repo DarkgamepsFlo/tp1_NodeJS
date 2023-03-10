136457f9e493d3fc49d7cc052006f28c859684b2
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2023-03-10 17:06:15 +0100

    Add files via upload
    Ajout du début de tp bot discord :
    Les 3 premières fonctions sont fonctionnelles et les 2 autres à moitié

---
+++ src/commands/server.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder} = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Réponse : nom du serveur et les membres'),
+    async execute(interaction) {
+        await interaction.reply("Nom du serveur : " + interaction.guild.name + "\nNombre de personne : " + interaction.guild.memberCount)
+    }
+}
