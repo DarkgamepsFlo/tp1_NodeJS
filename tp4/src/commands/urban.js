cac1ee526529ce0809288bd53705a70c9a9ec699
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2023-03-21 21:44:07 +0100

    Add files via upload
    Les fonctions cat et urban sont effectués, la liaison avec les API fonctionnent

---
+++ tp4/src/commands/urban.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js')
+const { request } = require('undici')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('urban')
+        .setDescription('Permet de montrer la définition de son nom dans urban dictionnary')
+        .addStringOption(option =>
+            option.setName('name')
+                .setDescription('Veuillez choisir le nom')
+                .setRequired(true)),
+    async execute(interaction) {
+        const name = interaction.options.getString("name");
+        const urbanResult = await request('https://api.urbandictionary.com/v0/define?term=' + name)
+        const { list } = JSON.parse(await urbanResult.body.text())
+                
+        await interaction.reply(list[0].definition)
+    },
+}
